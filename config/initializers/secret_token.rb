c2e10b37f73731273e5af7b1138534f100060a63
sresenthil <senthilaim007@gmail.com>
2013-09-10 13:12:18 +0530

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dpkgr::Application.config.secret_token = 'fe7c4e20f90bb11afe943a6dbbabcbabda36a989c5443a705e3ffa18960175e48facdff31a11d203eb1df53941d67c6e962079d9e628aea86184b0a68477cbfe'
